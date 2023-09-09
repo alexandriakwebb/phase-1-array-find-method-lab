4c322feacefe455300f1c9738b4b54b0b994b527
Alexandria Webb <alexandriakwebb@gmail.com>
2023-09-09 12:56:06 -0600

    passes test.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(array) {
+   let winningYear
+   if (winningYear = array.find(array => array.result === 'W')) {
+    return winningYear.year
+   } else {return undefined}
+}
